7280e65996704a2f08829903825755034b0b33e2
Denver Timlick <denvertimlick@gmail.com>
2025-09-12 16:50:13 -0500

    added js functions with broken array and error handling. Populated html

--- index.js
+++ index.js
@@ -14,6 +14,44 @@ const users = [
   { id: 10, name: "Padmé Amidala", age: 27 },
 ];
 
+//function to generate a list of names from any array
+function listNames(array) {
+  try {
+    array.forEach((item, index) => {
+      if (!("name" in item)) {
+        throw new Error(`Object at index ${index} is missing "name" property.`);
+      }
+    });
+    return array.map(item => item.name);
+  } catch (error) {
+    console.error("Error in retrieving names", error.message);
+  }
+}
+
+function ageFilter(array, maxAge) {
+  try{
+      return array
+        .filter(item => item.age < maxAge)
+        .map(item => `${item.name} (${item.age})`);
+  } catch (error) {
+    console.error("Error in retrieving names", error.message);
+  }
+}
+
+const brokenUsers = [
+  { id: 1, name: "Luke Skywalker", age: 23 },
+  { id: 2, name: "Darth Vader", age: 45 },
+  { id: 3, name: "Princess Leia", age: 23 },
+  { id: 4, age: 57 },
+  { id: 5, name: "Yoda", age: 900 },
+  { id: 6, name: "Han Solo", age: 32 },
+  { id: 7, name: "Chewbacca", age: 234 },
+  { id: 8, name: "R2-D2", age: 33 },
+  { id: 9, name: "C-3PO", age: 112 },
+  { id: 10, name: "Padmé Amidala", age: 27 },
+];
+
+
 // broken test data for exercise 6
 
 // 1. Print out the names of each character in the console, then render them in the HTML list with id "names-list"
